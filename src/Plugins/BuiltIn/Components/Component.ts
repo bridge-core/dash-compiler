cf11f846f0fe1d860bd1a1c7540ae2f32f829913
Outer Cloud <outercloudstudio@gmail.com>
2024-06-20 21:04:55 -0500

    Make sure all generated file types uses folder namespace

--- src/Plugins/BuiltIn/Components/Component.ts
+++ src/Plugins/BuiltIn/Components/Component.ts
@@ -43,24 +43,16 @@ export class Component {
 	}
 	//#endregion
 
-	async load(
-		jsRuntime: Runtime,
-		filePath: string,
-		type?: 'server' | 'client'
-	) {
+	async load(jsRuntime: Runtime, filePath: string, type?: 'server' | 'client') {
 		let v1CompatModule = { component: null }
 		const module = await jsRuntime
 			.run(filePath, {
 				defineComponent: (x: any) => x,
 				console: this.console,
-				Bridge: this.v1Compat
-					? v1Compat(v1CompatModule, this.fileType)
-					: undefined,
+				Bridge: this.v1Compat ? v1Compat(v1CompatModule, this.fileType) : undefined,
 			})
 			.catch((err) => {
-				this.console.error(
-					`Failed to execute component "${filePath}": ${err}`
-				)
+				this.console.error(`Failed to execute component "${filePath}": ${err}`)
 				return null
 			})
 		// Component execution failed
@@ -130,11 +122,7 @@ export class Component {
 		fileContent: any,
 		template: any,
 		location = `minecraft:${this.fileType}`,
-		operation?: (
-			deepMerge: (oldData: any, newData: any) => any,
-			oldData: any,
-			newData: any
-		) => any
+		operation?: (deepMerge: (oldData: any, newData: any) => any, oldData: any, newData: any) => any
 	) {
 		const keys = location.split('/')
 		const lastKey = keys.pop()!
@@ -142,10 +130,7 @@ export class Component {
 		const current = this.getObjAtLocation(fileContent, [...keys])
 
 		current[lastKey] = (
-			operation
-				? (oldData: any, newData: any) =>
-						operation(deepMerge, oldData, newData)
-				: deepMerge
+			operation ? (oldData: any, newData: any) => operation(deepMerge, oldData, newData) : deepMerge
 		)(current[lastKey] ?? {}, template ?? {})
 	}
 	protected getObjAtLocation(fileContent: any, location: string[]) {
@@ -169,54 +154,34 @@ export class Component {
 		return current
 	}
 
-	async processTemplates(
-		fileContent: any,
-		componentArgs: any,
-		location: string
-	) {
+	async processTemplates(fileContent: any, componentArgs: any, location: string) {
 		if (typeof this.template !== 'function') return
 
 		// Try getting file identifier
 		const identifier: string =
-			fileContent[`minecraft:${this.fileType}`]?.description
-				?.identifier ?? 'bridge:no_identifier'
+			fileContent[`minecraft:${this.fileType}`]?.description?.identifier ?? 'bridge:no_identifier'
 		// Used to compose the animation (controller) short name so the user knows how to reference their animation (controller)
 		const fileName = await hashString(`${this.name}/${identifier}`)
 
-		const projectNamespace =
-			this.projectConfig?.get()?.namespace ?? 'bridge'
-		let folderNamespace: string;
+		const projectNamespace = this.projectConfig?.get()?.namespace ?? 'bridge'
+		let folderNamespace: string
 		if (projectNamespace.includes('_')) {
-			const studioname = projectNamespace.split('_')[0];
+			const studioname = projectNamespace.split('_')[0]
 			const packname = projectNamespace.split('_').slice(1).join('_')
-			folderNamespace = `${studioname}/${packname}`;
+			folderNamespace = `${studioname}/${packname}`
 		} else {
-			folderNamespace = 'bridge';
+			folderNamespace = 'bridge'
 		}
 
 		// Setup animation/animationController helper
 		const animation = (animation: any, molangCondition?: string) => {
 			this.animations.push([animation, molangCondition])
-			return this.getShortAnimName(
-				'a',
-				fileName,
-				this.animations.length - 1
-			)
+			return this.getShortAnimName('a', fileName, this.animations.length - 1)
 		}
 
-		const animationController = (
-			animationController: any,
-			molangCondition?: string
-		) => {
-			this.animationControllers.push([
-				animationController,
-				molangCondition,
-			])
-			return this.getShortAnimName(
-				'ac',
-				fileName,
-				this.animationControllers.length - 1
-			)
+		const animationController = (animationController: any, molangCondition?: string) => {
+			this.animationControllers.push([animationController, molangCondition])
+			return this.getShortAnimName('ac', fileName, this.animationControllers.length - 1)
 		}
 
 		const lootTable = (lootTableDef: any) => {
@@ -243,44 +208,22 @@ export class Component {
 		}
 		const recipe = (recipeDef: any) => {
 			this.serverFiles.push([
-				`recipes/bridge/${this.getShortAnimName(
-					'recipe',
-					fileName,
-					this.serverFiles.length
-				)}.json`,
+				`recipes/${folderNamespace}/${this.getShortAnimName('recipe', fileName, this.serverFiles.length)}.json`,
 				recipeDef,
 			])
 		}
 		const spawnRule = (spawnRuleDef: any) => {
 			this.serverFiles.push([
-				`spawn_rules/bridge/${this.getShortAnimName(
-					'sr',
-					fileName,
-					this.serverFiles.length
-				)}.json`,
+				`spawn_rules/${folderNamespace}/${this.getShortAnimName('sr', fileName, this.serverFiles.length)}.json`,
 				spawnRuleDef,
 			])
 		}
 
-		const permutationEventName = (
-			await hashString(`${this.name}/${location}`)
-		).slice(0, 16)
+		const permutationEventName = (await hashString(`${this.name}/${location}`)).slice(0, 16)
 		const onActivated = (eventResponse: any) =>
-			this.registerLifecycleHook(
-				fileContent,
-				location,
-				eventResponse,
-				permutationEventName,
-				'activated'
-			)
+			this.registerLifecycleHook(fileContent, location, eventResponse, permutationEventName, 'activated')
 		const onDeactivated = (eventResponse: any) =>
-			this.registerLifecycleHook(
-				fileContent,
-				location,
-				eventResponse,
-				permutationEventName,
-				'deactivated'
-			)
+			this.registerLifecycleHook(fileContent, location, eventResponse, permutationEventName, 'deactivated')
 
 		// Execute template function with context for current fileType
 		if (this.fileType === 'entity') {
@@ -300,19 +243,15 @@ export class Component {
 				tradeTable,
 				spawnRule,
 				dialogueScene:
-					!this.targetVersion ||
-					compare(this.targetVersion, '1.17.10', '>=')
+					!this.targetVersion || compare(this.targetVersion, '1.17.10', '>=')
 						? (scene: any, openDialogue = true) => {
-								if (!this.dialogueScenes[fileName])
-									this.dialogueScenes[fileName] = []
+								if (!this.dialogueScenes[fileName]) this.dialogueScenes[fileName] = []
 								this.dialogueScenes[fileName].push(scene)
 
 								if (scene.scene_tag && openDialogue)
 									onActivated({
 										run_command: {
-											command: [
-												`/dialogue open @s @p ${scene.scene_tag}`,
-											],
+											command: [`/dialogue open @s @p ${scene.scene_tag}`],
 										},
 									})
 						  }
@@ -321,7 +260,7 @@ export class Component {
 				onDeactivated,
 				client: {
 					create: (clientEntity: any, formatVersion = '1.10.0') => {
-						this.clientFiles[`entity/bridge/${fileName}.json`] = {
+						this.clientFiles[`entity/${folderNamespace}/${fileName}.json`] = {
 							format_version: formatVersion,
 							'minecraft:client_entity': Object.assign(
 								{
@@ -351,16 +290,8 @@ export class Component {
 				player: {
 					animationController,
 					animation,
-					create: (
-						template: any,
-						location?: string,
-						operation?: any
-					) => {
-						this.createOnPlayer.push([
-							location ?? `minecraft:entity`,
-							template,
-							operation,
-						])
+					create: (template: any, location?: string, operation?: any) => {
+						this.createOnPlayer.push([location ?? `minecraft:entity`, template, operation])
 					},
 				},
 			})
@@ -381,23 +312,27 @@ export class Component {
 		}
 	}
 
-	async processAdditionalFiles(
-		filePath: string,
-		fileContent: any,
-		isPlayerFile = false
-	) {
-		const bpRoot =
-			this.projectConfig?.getRelativePackRoot('behaviorPack') ?? 'BP'
+	async processAdditionalFiles(filePath: string, fileContent: any, isPlayerFile = false) {
+		const bpRoot = this.projectConfig?.getRelativePackRoot('behaviorPack') ?? 'BP'
 		const rpRoot = this.projectConfig?.getRelativePackRoot('resourcePack')
 		// Try getting file identifier
 		const identifier = isPlayerFile
 			? 'minecraft:player'
-			: fileContent[`minecraft:${this.fileType}`]?.description
-					?.identifier ?? 'bridge:no_identifier'
+			: fileContent[`minecraft:${this.fileType}`]?.description?.identifier ?? 'bridge:no_identifier'
+
+		const projectNamespace = this.projectConfig?.get()?.namespace ?? 'bridge'
+		let folderNamespace: string
+		if (projectNamespace.includes('_')) {
+			const studioname = projectNamespace.split('_')[0]
+			const packname = projectNamespace.split('_').slice(1).join('_')
+			folderNamespace = `${studioname}/${packname}`
+		} else {
+			folderNamespace = 'bridge'
+		}
 
 		const fileName = (await hashString(`${this.name}/${identifier}`)).slice(0, 25)
-		const animFileName = `${bpRoot}/animations/bridge/${fileName}.json`
-		const animControllerFileName = `${bpRoot}/animation_controllers/bridge/${fileName}.json`
+		const animFileName = `${bpRoot}/animations/${folderNamespace}/${fileName}.json`
+		const animControllerFileName = `${bpRoot}/animation_controllers/${folderNamespace}/${fileName}.json`
 
 		if (identifier === 'minecraft:player') {
 			this.createOnPlayer.forEach(([location, template, operation]) => {
@@ -424,19 +359,15 @@ export class Component {
 				},
 				[animControllerFileName]: {
 					baseFile: filePath,
-					fileContent: this.createAnimationControllers(
-						fileName,
-						fileContent
-					),
+					fileContent: this.createAnimationControllers(fileName, fileContent),
 				},
 			}
 		}
 
 		return {
 			...anims,
-			[join(bpRoot, `dialogue/bridge/${fileName}.json`)]:
-				this.dialogueScenes[fileName] &&
-				this.dialogueScenes[fileName].length > 0
+			[join(bpRoot, `dialogue/${folderNamespace}/${fileName}.json`)]:
+				this.dialogueScenes[fileName] && this.dialogueScenes[fileName].length > 0
 					? {
 							baseFile: filePath,
 							fileContent: JSON.stringify(
@@ -461,19 +392,13 @@ export class Component {
 				])
 			),
 			...Object.fromEntries(
-				Object.entries(this.clientFiles).map(
-					([currFilePath, jsonContent]) => [
-						join(rpRoot!, currFilePath),
-						{
-							baseFile: filePath,
-							fileContent: JSON.stringify(
-								jsonContent,
-								null,
-								'\t'
-							),
-						},
-					]
-				)
+				Object.entries(this.clientFiles).map(([currFilePath, jsonContent]) => [
+					join(rpRoot!, currFilePath),
+					{
+						baseFile: filePath,
+						fileContent: JSON.stringify(jsonContent, null, '\t'),
+					},
+				])
 			),
 		}
 	}
@@ -481,8 +406,7 @@ export class Component {
 	protected createAnimations(fileName: string, fileContent: any) {
 		if (this.animations.length === 0) return
 
-		const projectNamespace =
-			this.projectConfig?.get()?.namespace ?? 'bridge'
+		const projectNamespace = this.projectConfig?.get()?.namespace ?? 'bridge'
 
 		let id = 0
 		const animations: any = { format_version: '1.10.0', animations: {} }
@@ -497,7 +421,7 @@ export class Component {
 			const animId = this.getAnimName('animation', projectNamespace, fileName, id)
 			// Create shorter reference to animId that's unique per entity
 			const shortAnimId = this.getShortAnimName('a', fileName, id)
-			
+
 			// Save animation to animations object
 			animations.animations[animId] = anim
 
@@ -519,11 +443,7 @@ export class Component {
 					fileContent,
 					{
 						scripts: {
-							animate: [
-								!condition
-									? shortAnimId
-									: { [shortAnimId]: condition },
-							],
+							animate: [!condition ? shortAnimId : { [shortAnimId]: condition }],
 						},
 					},
 					'minecraft:entity/description'
@@ -537,8 +457,7 @@ export class Component {
 	protected createAnimationControllers(fileName: string, fileContent: any) {
 		if (this.animationControllers.length === 0) return
 
-		const projectNamespace =
-			this.projectConfig?.get()?.namespace ?? 'bridge'
+		const projectNamespace = this.projectConfig?.get()?.namespace ?? 'bridge'
 
 		let id = 0
 		const animationControllers: any = {
@@ -553,12 +472,7 @@ export class Component {
 			}
 
 			// Create unique animId
-			const animId = this.getAnimName(
-				'controller.animation',
-				projectNamespace,
-				fileName,
-				id
-			)
+			const animId = this.getAnimName('controller.animation', projectNamespace, fileName, id)
 			// Create shorter reference to animId that's unique per entity
 			const shortAnimId = this.getShortAnimName('ac', fileName, id)
 
@@ -583,11 +497,7 @@ export class Component {
 					fileContent,
 					{
 						scripts: {
-							animate: [
-								!condition
-									? shortAnimId
-									: { [shortAnimId]: condition },
-							],
+							animate: [!condition ? shortAnimId : { [shortAnimId]: condition }],
 						},
 					},
 					'minecraft:entity/description'
@@ -616,25 +526,14 @@ export class Component {
 		permutationEventName: string,
 		type: 'activated' | 'deactivated'
 	) {
-		if (!fileContent[`minecraft:${this.fileType}`].events)
-			fileContent[`minecraft:${this.fileType}`].events = {}
+		if (!fileContent[`minecraft:${this.fileType}`].events) fileContent[`minecraft:${this.fileType}`].events = {}
 		const entityEvents = fileContent[`minecraft:${this.fileType}`].events
 
-		if (
-			type === 'activated' &&
-			location === `minecraft:${this.fileType}/components`
-		) {
-			if (!entityEvents['minecraft:entity_spawned'])
-				entityEvents['minecraft:entity_spawned'] = {}
+		if (type === 'activated' && location === `minecraft:${this.fileType}/components`) {
+			if (!entityEvents['minecraft:entity_spawned']) entityEvents['minecraft:entity_spawned'] = {}
 
-			this.addEventReponse(
-				entityEvents['minecraft:entity_spawned'],
-				eventResponse
-			)
-		} else if (
-			this.fileType === 'entity' &&
-			location.startsWith(`minecraft:${this.fileType}/component_groups/`)
-		) {
+			this.addEventReponse(entityEvents['minecraft:entity_spawned'], eventResponse)
+		} else if (this.fileType === 'entity' && location.startsWith(`minecraft:${this.fileType}/component_groups/`)) {
 			const componentGroupName = location.split('/').pop()!
 			const eventsWithReferences = this.findComponentGroupReferences(
 				entityEvents,
@@ -645,20 +544,13 @@ export class Component {
 			eventsWithReferences.forEach((eventWithReference) =>
 				this.addEventReponse(eventWithReference, eventResponse)
 			)
-		} else if (
-			location.startsWith(`minecraft:${this.fileType}/permutations/`)
-		) {
+		} else if (location.startsWith(`minecraft:${this.fileType}/permutations/`)) {
 			const keys = location.split('/')
-			if (keys.pop() !== 'components')
-				throw new Error(
-					'Invalid component location inside of permutation'
-				)
+			if (keys.pop() !== 'components') throw new Error('Invalid component location inside of permutation')
 
 			const permutation = this.getObjAtLocation(fileContent, [...keys])
 			const eventName = `bridge:${permutationEventName}_${type}_${
-				type === 'activated'
-					? this.lifecycleHookCount.activated++
-					: this.lifecycleHookCount.deactivated++
+				type === 'activated' ? this.lifecycleHookCount.activated++ : this.lifecycleHookCount.deactivated++
 			}`
 
 			if (permutation.condition)
@@ -670,9 +562,7 @@ export class Component {
 							},
 						},
 					},
-					type === 'activated'
-						? permutation.condition
-						: `!(${permutation.condition})`,
+					type === 'activated' ? permutation.condition : `!(${permutation.condition})`,
 				])
 
 			entityEvents[eventName] = eventResponse
@@ -703,11 +593,7 @@ export class Component {
 	 *
 	 * @returns An array of event objects which have references to the component group
 	 */
-	protected findComponentGroupReferences(
-		events: any,
-		type: 'add' | 'remove',
-		componentGroupName: string
-	) {
+	protected findComponentGroupReferences(events: any, type: 'add' | 'remove', componentGroupName: string) {
 		let eventsWithComponentGroups: any[] = []
 
 		for (const eventName in events) {
@@ -715,24 +601,15 @@ export class Component {
 
 			if (Array.isArray(event.sequence))
 				eventsWithComponentGroups.push(
-					...this.findComponentGroupReferences(
-						event.sequence,
-						type,
-						componentGroupName
-					)
+					...this.findComponentGroupReferences(event.sequence, type, componentGroupName)
 				)
 			else if (Array.isArray(event.randomize))
 				eventsWithComponentGroups.push(
-					...this.findComponentGroupReferences(
-						event.randomize,
-						type,
-						componentGroupName
-					)
+					...this.findComponentGroupReferences(event.randomize, type, componentGroupName)
 				)
 			else {
 				const componentGroups = event[type]?.component_groups ?? []
-				if (componentGroups.includes(componentGroupName))
-					eventsWithComponentGroups.push(event)
+				if (componentGroups.includes(componentGroupName)) eventsWithComponentGroups.push(event)
 			}
 		}
 
